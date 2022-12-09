8cbc0e8963aa9c84bb3188f94c9cf04e2a73a70a
Colin <colin.liaud@gmail.com>
2022-12-09 11:16:43 +0100

    ajout classe chambre

--- main.cpp
+++ main.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 #include "hotel.h"
 #include "client.h"
+#include "chambre.h"
 
 int main() {
-        Client client("utruna", "liaud", "colin");
-    client.updatePrenom("coline");
+        Client client("utruna", "liaud", "flo");
+    client.updatePrenom("colin");
     std::cout << client.prenom() << std::endl;
 
     Hotel H1("R2D2", "STAR WARS", "Listenburg", 100);
     std::cout << H1.identifiantHotel() << ", " << H1.nom() << " , " << H1.ville() << " , nombre de place : " << H1.nombrechambre()<< std::endl;
     H1.updatenombrechambre(200);
     
+    Chambre C1("R2D2", 2, 100);
+    std::cout << C1.numeroChambre() << ", " << C1.nombreLits() << " , " << C1.prix() << std::endl;
+    C1.updatePrix(200);
     return 0;
 }
 
